d282025d8e74bf3384fd38ff5c73984efe423cd4
thanhnv <nguyenthanh2303@gmail.com>
2021-08-21 11:30:49 +0700

    fix: update install lib

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Query State Lib
 
+### Install 
+ 
+> pip3 install QueryStateLib
 
 ### Example do query state batch at client
 
 * [Example used](_test/test_client_querier.py)
 
 Github : https://github.com/thanhnv2303/QueryStateLib"
+
--- _test/test_decode.py
+++ _test/test_decode.py
@@ -1,5 +1,5 @@
 from artifacts.abi_py.erc20_abi import ERC20_ABI
-from query_state_lib.base import decode_eth_call_data, decode_eth_call_balance_of, decode_eth_get_balance
+from query_state_lib.base.utils.decoder import decode_eth_call_data, decode_eth_call_balance_of, decode_eth_get_balance
 
 abi = ERC20_ABI
 fn_name = "balanceOf"
